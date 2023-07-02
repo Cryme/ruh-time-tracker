bce33b9fa4537df26964e1c40f3bf0916bb77595
cryme <swapmeholy@gmail.com>
2023-07-02 15:21:47 +0300

    .

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@
 
 mod project;
 
+use crate::project::{Backend, WorkingMode};
 use eframe::egui;
+use eframe::egui::{Align, Layout, RichText, Ui, Visuals};
+use std::ops::Rem;
+use std::time::Duration;
+use uuid::Uuid;
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -17,41 +22,168 @@ fn main() -> Result<(), eframe::Error> {
         initial_window_size: Some(egui::vec2(800.0, 400.0)),
         ..Default::default()
     };
+
     eframe::run_native(
         "Ruh Time Tracker", // unused title
         options,
-        Box::new(|_cc| Box::<MyApp>::default()),
+        Box::new(|cc| Box::<MyApp>::new(MyApp::init(cc))),
     )
 }
 
-#[derive(Default)]
-struct MyApp {}
+struct MyApp {
+    backend: Backend,
+    current_dialog: CurrentDialog,
+    current_name: String,
+}
 
-impl eframe::App for MyApp {
-    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
-        egui::Rgba::TRANSPARENT.to_array() // Make sure we don't paint anything behind the rounded corners
+enum CurrentDialog {
+    None,
+    AddProject,
+    AddSubject,
+}
+
+impl MyApp {
+    fn init(cc: &eframe::CreationContext<'_>) -> Self {
+        let context = cc.egui_ctx.clone();
+        std::thread::spawn(move || loop {
+            std::thread::sleep(Duration::from_millis(1000));
+            context.request_repaint();
+        });
+
+        Self {
+            backend: Backend::load(),
+            current_dialog: CurrentDialog::None,
+            current_name: "".to_string(),
+        }
     }
 
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        custom_window_frame(ctx, frame, "egui with custom frame", |ui| {
-            ui.label("This is just the contents of the window.");
-            ui.horizontal(|ui| {
-                ui.label("egui theme:");
-                egui::widgets::global_dark_light_mode_buttons(ui);
-            });
+    fn build_projects(&mut self, ui: &mut Ui) {
+        ui.set_min_width(400.0);
+        ui.set_max_width(400.0);
+
+        ui.vertical(|ui| {
+            for project in &self.backend.projects {
+                ui.horizontal(|ui| {
+                    let current_id = if let Some(cur_project) = &self.backend.current_project {
+                        cur_project.lock().unwrap().id
+                    } else {
+                        Uuid::new_v4()
+                    };
+
+                    let r_project = project.lock().unwrap();
+
+                    let mut text = RichText::new(&r_project.name);
+
+                    if &r_project.id == &current_id {
+                        text = text.strong();
+                    }
+
+                    if ui.button(text).clicked() {
+                        self.backend.current_project = Some(project.clone());
+                    }
+
+                    ui.label(format_duration(r_project.get_time()));
+                });
+
+                ui.add_space(5.0);
+            }
+
+            if ui.button("   +   ").clicked() {
+                self.current_dialog = CurrentDialog::AddProject;
+            }
+        });
+    }
+
+    fn build_subjects(&mut self, ui: &mut Ui) {
+        ui.set_min_width(400.0);
+        ui.set_max_width(400.0);
+
+        let Some(current_project) = &self.backend.current_project else {
+            return;
+        };
+
+        ui.vertical(|ui| {
+            let project = current_project.lock().unwrap();
+
+            for subject in &project.subjects {
+                ui.horizontal(|ui| {
+                    let current_id = if let Some(cur_subject) = &self.backend.current_subject {
+                        cur_subject.lock().unwrap().id
+                    } else {
+                        Uuid::new_v4()
+                    };
+
+                    let r_subject = subject.lock().unwrap();
+
+                    let mut text = RichText::new(&r_subject.name);
+
+                    if &r_subject.id == &current_id {
+                        text = text.strong();
+                    }
+
+                    if ui.button(text).clicked() {
+                        self.backend.current_subject = Some(subject.clone());
+                    }
+
+                    ui.label(format_duration(r_subject.duration));
+                });
+
+                ui.add_space(5.0);
+            }
+
+            if ui.button("   +   ").clicked() {
+                self.current_dialog = CurrentDialog::AddSubject;
+            }
         });
     }
 }
 
+fn format_duration(duration: Duration) -> String {
+    const HOUR_S: f64 = 60.0 * 60.0;
+
+    let spent = duration.as_secs_f64();
+
+    let hours = (spent / HOUR_S).round();
+    let minutes = (spent.rem(&HOUR_S) / 60.0).round();
+
+    let hours_d = if hours > 9.0 {
+        format!("{hours}")
+    } else {
+        format!("0{hours}")
+    };
+    let minutes_d = if minutes > 9.0 {
+        format!("{minutes}")
+    } else {
+        format!("0{minutes}")
+    };
+
+    format!(" {hours_d}:{minutes_d}")
+}
+
+fn custom_global_dark_light_mode_buttons(ui: &mut Ui) {
+    let mut visuals = ui.ctx().style().visuals.clone();
+    custom_light_dark_radio_buttons(&mut visuals, ui);
+    ui.ctx().set_visuals(visuals);
+}
+
+fn custom_light_dark_radio_buttons(vis: &mut Visuals, ui: &mut Ui) {
+    ui.with_layout(Layout::right_to_left(Align::Min), |ui| {
+        ui.horizontal(|ui| {
+            ui.selectable_value(vis, Visuals::light(), "☀");
+            ui.selectable_value(vis, Visuals::dark(), "🌙");
+        });
+    });
+}
+
 fn custom_window_frame(
     ctx: &egui::Context,
-    frame: &mut eframe::Frame,
-    title: &str,
+    _frame: &mut eframe::Frame,
+    _title: &str,
     add_contents: impl FnOnce(&mut egui::Ui),
 ) {
     use egui::*;
 
-    let panel_frame = egui::Frame {
+    let panel_frame = Frame {
         fill: ctx.style().visuals.window_fill(),
         rounding: 10.0.into(),
         stroke: ctx.style().visuals.widgets.noninteractive.fg_stroke,
@@ -62,14 +194,127 @@ fn custom_window_frame(
     CentralPanel::default().frame(panel_frame).show(ctx, |ui| {
         let app_rect = ui.max_rect();
 
+        let mut c = (*ctx.style()).clone();
+        c.text_styles.insert(
+            TextStyle::Button,
+            FontId::new(18.0, FontFamily::Proportional),
+        );
+        c.text_styles
+            .insert(TextStyle::Body, FontId::new(18.0, FontFamily::Proportional));
+        ctx.set_style(c);
+
         // Add the contents:
-        let content_rect = {
-            let mut rect = app_rect;
-            rect
-        }
-        .shrink(4.0);
+        let content_rect = { app_rect }.shrink(4.0);
 
         let mut content_ui = ui.child_ui(content_rect, *ui.layout());
         add_contents(&mut content_ui);
     });
+}
+
+impl eframe::App for MyApp {
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        egui::Rgba::TRANSPARENT.to_array() // Make sure we don't paint anything behind the rounded corners
+    }
+
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        custom_window_frame(ctx, frame, "_", |ui| {
+            ui.horizontal_top(|ui| {
+                ui.label(format!(
+                    "Current work: {}",
+                    self.backend.get_current_work_name()
+                ));
+                custom_global_dark_light_mode_buttons(ui);
+            });
+
+            ui.horizontal(|ui| {
+                ui.set_min_height(55.0);
+                ui.set_max_height(55.0);
+
+                if self.backend.current_subject.is_some() {
+                    match self.backend.working_mode {
+                        WorkingMode::Idle => {
+                            if ui.button("START").clicked() {
+                                self.backend.start_subject();
+                            }
+                        }
+                        WorkingMode::InProgress(_) => {
+                            if ui.button("PAUSE").clicked() {
+                                self.backend.stop_subject();
+                            }
+                        }
+                    }
+                    ui.label(format_duration(self.backend.current_session_duration));
+                }
+            });
+
+            ui.separator();
+
+            ui.horizontal(|ui| {
+                ui.set_min_height(290.0);
+                ui.set_max_height(290.0);
+
+                ui.push_id(1, |ui| {
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        self.build_projects(ui);
+                    });
+                });
+
+                ui.separator();
+
+                ui.push_id(2, |ui| {
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        self.build_subjects(ui);
+                    });
+                });
+            });
+        });
+
+        self.backend.update_time();
+
+        match self.current_dialog {
+            CurrentDialog::None => {}
+            CurrentDialog::AddProject => {
+                egui::Window::new("Add Project")
+                    .collapsible(false)
+                    .resizable(false)
+                    .show(ctx, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.add(egui::TextEdit::singleline(&mut self.current_name));
+
+                            if ui.button("Cancel").clicked() {
+                                self.current_dialog = CurrentDialog::None;
+                                self.current_name = "".to_string();
+                            }
+
+                            if ui.button("Add").clicked() {
+                                self.current_dialog = CurrentDialog::None;
+                                self.backend.add_project(&*self.current_name);
+                                self.current_name = "".to_string();
+                            }
+                        });
+                    });
+            }
+            CurrentDialog::AddSubject => {
+                egui::Window::new("Add Project")
+                    .collapsible(false)
+                    .resizable(false)
+                    .show(ctx, |ui| {
+                        ui.horizontal(|ui| {
+                            ui.add(egui::TextEdit::singleline(&mut self.current_name));
+
+                            if ui.button("Cancel").clicked() {
+                                self.current_dialog = CurrentDialog::None;
+                                self.current_name = "".to_string();
+                            }
+
+                            if ui.button("Add").clicked() {
+                                self.current_dialog = CurrentDialog::None;
+                                self.backend.add_subject(&*self.current_name);
+                                self.current_name = "".to_string();
+                            }
+                        });
+                    });
+            }
+        }
+    }
 }
--- src/project.rs
+++ src/project.rs
@@ -1,57 +1,191 @@
-use std::sync::{Arc};
-use std::time::Duration;
-use eframe::egui::Key::D;
-use uuid::Uuid;
-use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, SystemTime};
 
-#[derive(Serialize, Deserialize)]
-pub struct Project {
-    #[serde(with = "my_uuid")]
-    id: Uuid,
-    name: String,
-    #[serde(with = "my_vec")]
-    subjects: Vec<Arc<Subject>>,
-    #[serde(skip)]
-    current_subject: Option<Arc<Subject>>
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Default)]
+pub enum WorkingMode {
+    #[default]
+    Idle,
+    InProgress(WorkingProgress),
 }
 
+pub struct WorkingProgress {
+    subject: Arc<Mutex<Subject>>,
+    previous_tick: SystemTime,
+}
 
-impl Project {
-    pub fn create(name: &str) -> Self {
-        Project{
-            id: Uuid::new_v4(),
-            name: name.to_string(),
-            subjects: Vec::new(),
+impl WorkingProgress {
+    fn start(subject: Arc<Mutex<Subject>>) -> Self {
+        Self {
+            subject,
+            previous_tick: SystemTime::now(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Backend {
+    #[serde(with = "my_vec")]
+    pub(crate) projects: Vec<Arc<Mutex<Project>>>,
+    #[serde(skip)]
+    pub(crate) current_project: Option<Arc<Mutex<Project>>>,
+    #[serde(skip)]
+    pub(crate) current_subject: Option<Arc<Mutex<Subject>>>,
+    #[serde(skip)]
+    pub(crate) working_mode: WorkingMode,
+    pub(crate) current_session_duration: Duration,
+    #[serde(with = "my_uuid")]
+    pub(crate) last_session_project_id: Uuid,
+    last_save: SystemTime,
+}
+
+impl Backend {
+    pub fn load() -> Self {
+        let config = Path::new("./data.ron");
+
+        if config.exists() {
+            if let Ok(mut file) = File::open("./data.ron") {
+                let mut contents = String::new();
+                if let Ok(_) = file.read_to_string(&mut contents) {
+                    if let Ok(data) = ron::from_str(&*contents) {
+                        return data;
+                    }
+                }
+            }
+        }
+
+        Self {
+            projects: vec![],
+            current_project: None,
             current_subject: None,
+            working_mode: Default::default(),
+            current_session_duration: Duration::default(),
+            last_session_project_id: Uuid::new_v4(),
+            last_save: SystemTime::now(),
         }
     }
 
-    pub fn select_subject(&mut self, index: usize) {
-        if let Some(subject) = self.subjects.get(index){
-            self.current_subject = Some(subject.clone());
+    fn dump(&mut self) {
+        let mut file = File::create("./data.ron").unwrap();
+        file.write_all(
+            ron::ser::to_string_pretty(self, ron::ser::PrettyConfig::default())
+                .unwrap()
+                .as_bytes(),
+        )
+        .unwrap();
+        self.last_save = SystemTime::now();
+    }
+
+    pub fn update_time(&mut self) {
+        if let WorkingMode::InProgress(progress) = &mut self.working_mode {
+            let duration = SystemTime::now()
+                .duration_since(progress.previous_tick)
+                .unwrap();
+
+            progress.previous_tick = SystemTime::now();
+
+            self.current_session_duration += duration;
+
+            progress.subject.lock().unwrap().duration += duration;
+
+            if SystemTime::now().duration_since(self.last_save).unwrap() > Duration::from_secs(1) {
+                self.dump();
+            }
+        }
+    }
+
+    pub fn get_current_work_name(&self) -> String {
+        if let Some(subject) = &self.current_subject {
+            if let Some(project) = &self.current_project {
+                return format!(
+                    " {} - {}",
+                    project.lock().unwrap().name,
+                    subject.lock().unwrap().name
+                );
+            }
         }
 
+        return "None".to_string();
+    }
+
+    pub fn add_project(&mut self, name: &str) {
+        self.projects
+            .push(Arc::new(Mutex::new(Project::create(name))));
+
+        self.dump();
     }
 
     pub fn add_subject(&mut self, name: &str) {
-        self.subjects.push(
-            Arc::new(Subject::create(name))
-        )
+        if let Some(project) = &self.current_project {
+            project.lock().unwrap().add_subject(name);
+
+            self.dump();
+        }
+    }
+
+    pub fn start_subject(&mut self) {
+        if let Some(subject) = &self.current_subject {
+            if self.last_session_project_id != subject.lock().unwrap().id {
+                self.current_session_duration = Duration::ZERO;
+            }
+
+            self.last_session_project_id = subject.lock().unwrap().id;
+
+            self.working_mode = WorkingMode::InProgress(WorkingProgress::start(subject.clone()));
+        }
+    }
+
+    pub fn stop_subject(&mut self) {
+        self.working_mode = WorkingMode::Idle;
     }
 }
 
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Project {
+    #[serde(with = "my_uuid")]
+    pub(crate) id: Uuid,
+    pub(crate) name: String,
+    #[serde(with = "my_vec")]
+    pub(crate) subjects: Vec<Arc<Mutex<Subject>>>,
+}
+
+impl Project {
+    pub fn create(name: &str) -> Self {
+        Project {
+            id: Uuid::new_v4(),
+            name: name.to_string(),
+            subjects: Vec::new(),
+        }
+    }
+
+    pub fn add_subject(&mut self, name: &str) {
+        self.subjects
+            .push(Arc::new(Mutex::new(Subject::create(name))))
+    }
+
+    pub fn get_time(&self) -> Duration {
+        self.subjects
+            .iter()
+            .fold(Duration::default(), |s, e| s + e.lock().unwrap().duration)
+    }
+}
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Subject {
     #[serde(with = "my_uuid")]
-    id: Uuid,
-    name: String,
-    duration: Duration,
+    pub(crate) id: Uuid,
+    pub(crate) name: String,
+    pub(crate) duration: Duration,
 }
 
 impl Subject {
     fn create(name: &str) -> Self {
-        Subject{
+        Subject {
             id: Uuid::new_v4(),
             name: name.to_string(),
             duration: Duration::default(),
@@ -59,52 +193,55 @@ impl Subject {
     }
 }
 
-
 mod my_vec {
-    use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
-    use std::str::FromStr;
-    use std::sync::Arc;
-    use crate::project::Subject;
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+    use std::sync::{Arc, Mutex};
 
-    pub fn serialize<S>(val: &Vec<Arc<Subject>>, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
+    pub fn serialize<S, T>(val: &Vec<Arc<Mutex<T>>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        T: Serialize,
+        T: Clone,
     {
         let mut res = Vec::new();
 
-        for v in val{
-            let n = (**v).clone();
+        for v in val {
+            let n = v.lock().unwrap().clone();
             res.push(n)
         }
 
         res.serialize(serializer)
     }
 
-    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<Arc<Subject>>, D::Error>
-        where
-            D: Deserializer<'de>,
+    pub fn deserialize<'de, D, T>(deserializer: D) -> Result<Vec<Arc<Mutex<T>>>, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: Deserialize<'de>,
+        T: Clone,
     {
-        let val: Vec<Subject> = Deserialize::deserialize(deserializer)?;
+        let val: Vec<T> = Deserialize::deserialize(deserializer)?;
 
-        Ok(val.iter().map(|v|  Arc::new(v.clone())).collect())
+        Ok(val
+            .iter()
+            .map(|v| Arc::new(Mutex::new(v.clone())))
+            .collect())
     }
 }
 
 mod my_uuid {
-    use uuid::Uuid;
     use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
-    use std::str::FromStr;
+    use uuid::Uuid;
 
     pub fn serialize<S>(val: &Uuid, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
+    where
+        S: Serializer,
     {
         val.to_string().serialize(serializer)
     }
 
     pub fn deserialize<'de, D>(deserializer: D) -> Result<Uuid, D::Error>
-        where
-            D: Deserializer<'de>,
+    where
+        D: Deserializer<'de>,
     {
         let val: &str = Deserialize::deserialize(deserializer)?;
         Uuid::parse_str(val).map_err(D::Error::custom)
